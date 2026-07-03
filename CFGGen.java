e1031914ddc09fefb6a7524000b0fba697625c5a
Se Behan <behansh@whs.school.nz>
2026-07-03 14:28:16 +1200

    Cfg gen work

--- CFGGen.java
+++ CFGGen.java
@@ -8,6 +8,26 @@
 public class CFGGen
 {
     public CFGGen(){
+        int cfgLength = 20;
+        String name;
+        int teacher;
+        int time;
+        for(int i = 0;i<cfgLength;i++){
+            name = genName();
+            teacher = genTeacher();
+            time = genTime();
+        }
+    }
+    
+    public String genName(){
+        
+    }
+    
+    public int genTeacher(){
+        
+    }
+    
+    public int genTime(){
         
     }
 }
